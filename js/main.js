7117b0508d5e4472cc515e1e5f48ee4112fb65f4
tymoreau <typhaine.moreau@insa-lyon.fr>
2016-02-23 16:01:50 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+var renderer = new THREE.WebGLRenderer();
+
